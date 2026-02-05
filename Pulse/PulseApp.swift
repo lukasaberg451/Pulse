b7769a9e467c097808d4ae27502caa34842cbf45
lukasaberg451 <135319943+lukasaberg451@users.noreply.github.com>
2026-02-05 13:43:30 +0100

    Initial Commit

---
+++ Pulse/PulseApp.swift
@@ -0,0 +1,17 @@
+//
+//  PulseApp.swift
+//  Pulse
+//
+//  Created by user291690 on 2/5/26.
+//
+
+import SwiftUI
+
+@main
+struct PulseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
